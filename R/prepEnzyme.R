a128f7bd8e8729566afcf65cfdce9aac390ae899
EmilyB17 <ebean@uwyo.edu>
2017-12-11 11:13:33 -0700

    commit

---
+++ R/prepEnzyme.R
@@ -0,0 +1,13 @@
+# writing function to transform Emily enzyme data to be workable
+
+# data = dataframe of enzyme data 
+prepEnzyme <- function(data) {
+  data$SampDate <- NULL
+  data$RunDay <- NULL
+  data$Plot <- as.factor(data$Plot)
+  data$Block <- factor(data$Block, levels = c("1", "2", "3", "4"), ordered = TRUE)
+  data$Treatment <- as.factor(data$Treatment)
+  data$GrazeTime <- factor(data$GrazeTime, levels = c("PRE", "24H", "1WK", "4WK"), ordered = TRUE)
+  data$Sample <- as.factor(data$Sample)
+  return(data)
+}
